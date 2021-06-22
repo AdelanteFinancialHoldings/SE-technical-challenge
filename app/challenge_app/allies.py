9d133a00a52315c6b4a500166ab65fa16b77654d
SebsCubs <ascespedesc@unal.edu.co>
2021-06-22 11:42:57 -0500

    Merging endpoints into one, cleaning unused lines.

--- app/challenge_app/allies.py
+++ app/challenge_app/allies.py
@@ -6,72 +6,53 @@ from .db import get_db
 
 bp = Blueprint('allies', __name__, url_prefix='/allies')
 
-def ally_exists(allyId):
-    return True
-
-def validate_credentials(username,password):
-    return True
-
-def add_credentials_to_ally(username,password):
-    return "Credentials added"
-
 
 
 @bp.route('/')
 def index():
     return "Support engineer technical challenge API"
 
-@bp.route('/<allyId>/add-credentials', methods=['POST'])
-def add_credentials(allyId):
-    error = None
-    username = request.json['username']
-    password = request.json['password']
-    db = get_db()
-    allyName = None
-
-    if not username:
-        error = 'Username and password are required.'
-        return jsonify(message=error),400
-    elif not password:
-        error = 'Password is required.'
-        return jsonify(message=error),400
-    else:
+@bp.route('/<allyId>/credentials',methods=['GET','POST'])
+def credentials(allyId):
+    if request.method == 'GET':
+        db = get_db()
         with db:
             with db.cursor() as curs:
                 curs.execute("SELECT * FROM public.stores WHERE id = %s",(allyId,))
                 result = curs.fetchone()
                 if result == None:
-                    error = 'Ally not found'
-                    return jsonify(message=error),404   
+                    return jsonify(message='Ally not found',status=404),404   
+                elif(result[1]['credentials'] != None):
+                    return jsonify(message="Ally has credentials set!"),200
                 else:
-                    hashed_creds = '"%s"'%generate_password_hash(password)
-                    allyName=result[1]['name']
-                    curs.execute("UPDATE public.stores s SET ""data""=jsonb_set(to_jsonb(s.""data""),'"+"{"+"credentials"+"}"+"',%s,false) WHERE id=%s;",(hashed_creds,allyId))
-            db.commit()
-            return jsonify(message="Credentials added",
-                AllyId=allyId,
-                AllyName=allyName),200
+                    return jsonify(message="Ally does not have credentials set!"),400
+    elif request.method == 'POST':
+        username = request.json['username']
+        password = request.json['password']
+        db = get_db()
+        allyName = None
 
-    
-
-
-
-@bp.route('/<allyId>/is-active',methods=['GET'])
-def ally_status(allyId):
-    #TODO: Chequear si la variable de entorno es válida.
-    error = None
-    db = get_db()
-    with db:
-        with db.cursor() as curs:
-            curs.execute("SELECT * FROM public.stores WHERE id = %s",(allyId,))
-            result = curs.fetchone()
-            if result == None:
-                error = 'Ally not found'
-                return jsonify(message=error,status=404),404   
-            elif(result[1]['credentials'] != None):
-                return jsonify(message="Ally has credentials set!"),200
-            else:
-                return jsonify(message="Ally does not have credentials set!"),400
+        if not username:
+            return jsonify(message="Username and password are required."),400
+        elif not password:
+            return jsonify(message="Password is required."),400
+        else:
+            with db:
+                with db.cursor() as curs:
+                    curs.execute("SELECT * FROM public.stores WHERE id = %s",(allyId,))
+                    result = curs.fetchone()
+                    if result == None:
+                        return jsonify(message="Ally not found"),404   
+                    else:
+                        hashed_creds = '"%s"'%generate_password_hash(password)
+                        allyName=result[1]['name']
+                        curs.execute("UPDATE public.stores s SET ""data""=jsonb_set(to_jsonb(s.""data""),'"+"{"+"credentials"+"}"+"',%s,false) WHERE id=%s;",(hashed_creds,allyId))
+                db.commit()
+                return jsonify(message="Credentials added",
+                    AllyId=allyId,
+                    AllyName=allyName),200
+    else:
+        return jsonify(message= "It was not possible to process your request, check the available methods"), 400
 
 
 @bp.errorhandler(400)
