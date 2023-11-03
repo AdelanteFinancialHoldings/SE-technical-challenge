a103c3aaa81e1b55f2b40a89ec7a10f0bfcc6688
Alex Bacelar <abacelar@addi.com>
2023-11-02 21:47:53 -0300

    Change in the configs for Docker compose

--- app/challenge_app/db.py
+++ app/challenge_app/db.py
@@ -1,8 +1,6 @@
 import psycopg2
 import psycopg2.extras
-import click
-from flask import current_app, g
-from flask.cli import with_appcontext
+from flask import g
 
 def get_db():
     if 'db' not in g:
